867c54e39488e55bbe5e7a9d69579da944c0859a
324300979 <yaelarbel2002@gmail.com>
2025-11-01 18:25:01 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -7,8 +7,7 @@ def find_max_number(num1, num2, num3):
       return num3
 
 def find_mean(num1, num2, num3):
-    a = (num1+num2+num3)/3
-     return a
+    return (num1+num2+num3)/3
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
@@ -16,5 +15,5 @@ def find_mean_std(num1, num2, num3):
     b = num2 - mean
     c = num3 - mean
     d = a**2 + b**2 + c**2
-    std = (d/3)**1/2
-     return (mean, std)
+    std = (d/3)**(1/2)
+    return (mean, std)
